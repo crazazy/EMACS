4830d2ce4a0fb86342a4de2505e9fe279b27838a
Erik Oosting <erik99.oosting@gmail.com>
2020-10-31 13:21:51 +0100

    automated package blacklisting

--- default.nix
+++ default.nix
@@ -5,33 +5,19 @@ let
   srcFiles = attrNames (readDir ./elisp);
   bigConfigStr = concatStringsSep "\n" (map (n: readFile (./elisp + ("/" + n))) srcFiles);
   bigConfig = toFile "emacsrc" bigConfigStr;
-  # packages which aren't available in the nix-repositories, mostly because they're built-in, or built into another package
-  blackList = [
-    "cc-mode"
-    "company-tern"
-    "css-mode"
-    "ede"
-    "ediff"
-    "flycheck-mix"
-    "linum"
-    "recentf"
-    "semantic"
-    "windmove"
-  ];
   customConfig = runCommand "config.el" { } ''
     mkdir -p $out/share/emacs/site-lisp
     cp ${bigConfig} $out/share/emacs/site-lisp/default.el
   '';
-  deps = runCommand "deps.nix" { } (''
+  deps = runCommand "deps.nix" { } ''
     cat > $out << EOF
-    config: epkgs: with (epkgs // epkgs.melpaPackages); [
-    config
-    use-package
+    config: epkgs: with epkgs; [ config ] ++ map (e: builtins.getAttr e epkgs) (builtins.filter (v: builtins.hasAttr v epkgs)[
+    "use-package"
     EOF
 
-    cat ${bigConfig} | grep -o 'use-package [a-z0-9\-]*' | cut -d ' ' -f 2 >> $out
-    echo ] >> $out
-  '' + (concatStringsSep "\n" (map (n: "sed -i /${n}/d $out") blackList)));
+    cat ${bigConfig} | grep -o 'use-package [a-z0-9\-]*' | sed -e 's/[a-z0-9\-]*$/"\0"/g' | cut -d ' ' -f 2 >> $out
+    echo "])" >> $out
+  '';
   emacs = emacsWithPackages (import (deps.outPath) customConfig);
 in
 emacs
