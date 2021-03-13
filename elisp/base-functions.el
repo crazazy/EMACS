9cd6f1783591cd83cfd4d432cc375ffb4a1a194e
Erik Oosting <erik99.oosting@gmail.com>
2021-03-13 10:45:00 +0100

    added create-scratch-buffer function

--- elisp/base-functions.el
+++ elisp/base-functions.el
@@ -2,6 +2,10 @@
 
 ;; (defun something
 ;;    (do-something))
+(defun create-scratch-buffer nil
+    "create a scratch buffer"
+    (interactive)
+    (switch-to-buffer (get-buffer-create "*scratch*"))
+    (lisp-interaction-mode))
 
 (provide 'base-functions)
-
