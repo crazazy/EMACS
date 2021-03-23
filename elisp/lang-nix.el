0b95961fe4e0625ba4790ddfd09e8a30f7ea9be8
Erik Oosting <erik99.oosting@gmail.com>
2021-03-23 18:38:11 +0100

    add company backend for nix

--- elisp/lang-nix.el
+++ elisp/lang-nix.el
@@ -1,9 +1,16 @@
 (use-package nix-mode
   :mode "\\.nix\\'"
+  :config
+
+  ;; the company-nix backend is not available in melpa, but has no new dependencies
+  (unless (package-installed-p 'company-nix)
+    (with-temp-buffer
+      (url-insert-file-contents "https://github.com/NixOS/nix-mode/raw/master/nix-company.el")
+      (eval-buffer)))
+  (add-hook 'nix-mode-hook (lambda ()
+			     (set (make-local-variable 'company-backends)
+				  '((company-nix)))))
+  (add-hook 'nix-mode-hook 'company-mode)
   )
-(use-package company-nixos-options
-  :after company-mode
-  :init
-  (add-to-list 'company-backends 'company-nixos-options))
 
 (provide 'lang-nix)
