822dc70ba92cbe0daa6c37d4024c838dd22a2f5e
Erik Oosting <erik99.oosting@gmail.com>
2021-04-03 15:59:02 +0200

    add functions to quickly join irc chats

--- elisp/base-functions.el
+++ elisp/base-functions.el
@@ -8,4 +8,19 @@
     (switch-to-buffer (get-buffer-create "*scratch*"))
     (lisp-interaction-mode))
 
+;; irc quick logins
+(defmacro irc-quickjoin (serverfunc url &optional nick)
+  "create a function to quickly join a server"
+  `(defun ,serverfunc (password)
+     (interactive (list (password-read "Password: ")))
+     (erc-tls :server ,url
+	      :nick ,(if nick
+			 nick
+		       "crazazy")
+	      :port 6697
+	      :password password)))
+
+(irc-quickjoin tilde-irc "eu.tilde.chat")
+(irc-quickjoin freenode-irc "irc.freenode.net")
+
 (provide 'base-functions)
