cf5ae96ffb41c697700bc487251f8f2ac4cc655a
Erik Oosting <erik99.oosting@gmail.com>
2021-05-13 13:02:34 +0200

    add a nix sandbox generator

--- elisp/base-functions.el
+++ elisp/base-functions.el
@@ -2,6 +2,32 @@
 
 ;; (defun something
 ;;    (do-something))
+(defun intersperse (el ls)
+  "put element EL inbetween each element of list LS"
+  (if (cdr ls)
+      (cons (car ls) (cons el (intersperse el (cdr ls))))
+    ls))
+
+(defun nix-env-from-packages (name &rest packages)
+  "create a nix environment from nix packages. returns the location of the environment"
+  (interactive (append
+		(list (read-string "Environment name: " nil nil "envless"))
+		(split-string (read-string "Packages: "))))
+  (require 'nix-sandbox)
+  (with-temp-buffer
+    (insert (format "
+{ pkgs ? import %s {}}:
+with pkgs;
+buildEnv {
+name = %s;
+paths = [
+%s
+];
+}
+    " (or nix-nixpkgs-path "<nixpkgs>") name (apply 'concat (intersperse "\n" packages))))
+    (write-file (concat temporary-file-directory name "-env/default.nix"))
+    (concat temporary-file-directory name "-env")))
+
 (defun create-scratch-buffer nil
     "create a scratch buffer"
     (interactive)
