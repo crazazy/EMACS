0fd086f9c43d86e4e4d5f01ededa81353e8eddb1
Erik Oosting <erik99.oosting@gmail.com>
2021-05-13 15:31:12 +0200

    integrated nix-sandbox in haskell

--- elisp/lang-haskell.el
+++ elisp/lang-haskell.el
@@ -13,19 +13,31 @@
   (add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
   (add-hook 'haskell-mode-hook 'company-mode)
   (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
+  (add-hook 'haskell-mode-hook (lambda ()
+				 (set (make-local-variable 'nix-buffer-sandbox
+							   (nix-find-sandbox (nix-env-from-packages "Haskell"
+												    "ghc"
+												    "stack"
+												    "cabal-install"
+												    "haskellPackages.fourmolu"))))))
 
   ;; intero-mode for a complete IDE solution to haskell
   ;; commercialhaskell.github.io/intero
   (use-package dante
-    :config (add-hook 'haskell-mode-hook 'dante-mode))
+    :config
+    (add-hook 'haskell-mode-hook 'dante-mode))
 
   ;; hindent - format haskell code automatically
   ;; https://github.com/chrisdone/hindent
-  (when (executable-find "ormolu")
+  (when (nix-executable-find nix-buffer-sandbox "fourmolu")
     (use-package ormolu
 	:hook (haskell-mode . ormolu-format-on-save-mode)
 	:bind
 	(:map haskell-mode-map
-	    ("C-c r" . ormolu-format-buffer)))))
+	      ("C-c r" . ormolu-format-buffer))
+	:config
+	(add-hook 'ormolu-format-on-save-mode-hook (lambda ()
+						     (set (make-local-variable 'ormolu-process-path
+									       (nix-executable-find nix-buffer-sandbox "fourmolu"))))))))
 
 (provide 'lang-haskell)
