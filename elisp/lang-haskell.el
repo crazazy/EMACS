2e1956d7e48783c008cf6c1a7f3170376ae5ba1e
Erik Oosting <erik99.oosting@gmail.com>
2021-05-15 14:32:01 +0200

    fix haskell

--- elisp/lang-haskell.el
+++ elisp/lang-haskell.el
@@ -2,6 +2,7 @@
 ;; https://github.com/haskell/haskell-mode
 (use-package haskell-mode
   ;; haskell-mode swaps `C-m' and `C-j' behavior. Revert it back
+  :after nix-sandbox
   :bind (:map haskell-mode-map
               ("C-m" . newline)
               ("C-j" . electric-newline-and-maybe-indent))
@@ -10,34 +11,39 @@
     "Hook for `haskell-mode'."
     (set (make-local-variable 'company-backends)
          '((dante-company company-files))))
+  (setq haskell-sandbox (nix-env-from-packages "Haskell"
+					       "ghc"
+					       "stack"
+					       "cabal-install"
+					       "haskellPackages.fourmolu"))
+  (setq haskell-process-wrapper-function
+	(lambda (args)
+	  (cons
+	   (nix-executable-find haskell-sandbox (car args))
+	   (cdr args))))
   (add-hook 'haskell-mode-hook 'my-haskell-mode-hook)
   (add-hook 'haskell-mode-hook 'company-mode)
   (add-hook 'haskell-mode-hook 'haskell-indentation-mode)
-  (add-hook 'haskell-mode-hook (lambda ()
-				 (set (make-local-variable 'nix-buffer-sandbox
-							   (nix-find-sandbox (nix-env-from-packages "Haskell"
-												    "ghc"
-												    "stack"
-												    "cabal-install"
-												    "haskellPackages.fourmolu"))))))
 
   ;; intero-mode for a complete IDE solution to haskell
   ;; commercialhaskell.github.io/intero
   (use-package dante
     :config
+    (setq dante-repl-command-line (list (nix-executable-find haskell-sandbox "ghci")))
     (add-hook 'haskell-mode-hook 'dante-mode))
 
   ;; hindent - format haskell code automatically
   ;; https://github.com/chrisdone/hindent
-  (when (nix-executable-find nix-buffer-sandbox "fourmolu")
+  (when (nix-executable-find haskell-sandbox "fourmolu")
     (use-package ormolu
 	:hook (haskell-mode . ormolu-format-on-save-mode)
 	:bind
 	(:map haskell-mode-map
 	      ("C-c r" . ormolu-format-buffer))
 	:config
-	(add-hook 'ormolu-format-on-save-mode-hook (lambda ()
-						     (set (make-local-variable 'ormolu-process-path
-									       (nix-executable-find nix-buffer-sandbox "fourmolu"))))))))
+	(add-hook 'ormolu-format-on-save-mode-hook
+		  (lambda ()
+		    (set (make-local-variable 'ormolu-process-path)
+			 (nix-executable-find haskell-sandbox "fourmolu")))))))
 
 (provide 'lang-haskell)
