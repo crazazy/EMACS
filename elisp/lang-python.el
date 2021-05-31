9973ece967ff57e8b54599c1f1356cfe1d31856d
Erik Oosting <erik99.oosting@gmail.com>
2021-05-31 19:23:18 +0200

    made the python sandbox not global anymore

--- elisp/lang-python.el
+++ elisp/lang-python.el
@@ -23,7 +23,21 @@
     :bind (:map elpy-mode-map
 	      ("M-." . elpy-goto-definition)
 	      ("M-," . pop-tag-mark)))
-  (elpy-enable))
+  (elpy-enable)
+  (use-package pyenv-mode
+    :after python nix-sandbox
+    :if
+    (nix-executable-find python-sandbox "pyenv")
+    :init
+    (add-to-list 'exec-path "~/.pyenv/shims")
+    (setenv "WORKON_HOME" "~/.pyenv/versions/")
+    :config
+    (pyenv-mode)
+    :bind
+    ("C-x p e" . pyenv-activate-current-project))
+
+  (add-hook 'after-init-hook 'pyenv-init)
+  (add-hook 'projectile-after-switch-project-hook 'pyenv-activate-current-project))
 
 (use-package pip-requirements
   :config
@@ -32,18 +46,6 @@
 (use-package py-autopep8)
 
 
-(use-package pyenv-mode
-  :after python nix-sandbox
-  :if
-  (nix-executable-find python-sandbox "pyenv")
-  :init
-  (add-to-list 'exec-path "~/.pyenv/shims")
-  (setenv "WORKON_HOME" "~/.pyenv/versions/")
-  :config
-  (pyenv-mode)
-  :bind
-  ("C-x p e" . pyenv-activate-current-project))
-
 (defun pyenv-init()
   (setq global-pyenv (replace-regexp-in-string "\n" "" (shell-command-to-string "pyenv global")))
   (message (concat "Setting pyenv version to " global-pyenv))
@@ -51,21 +53,19 @@
   (defvar pyenv-current-version nil global-pyenv))
 
 (defun pyenv-activate-current-project ()
-  "Automatically activates pyenv version if .python-version file exists."
+"Automatically activates pyenv version if .python-version file exists."
   (interactive)
   (f-traverse-upwards
-   (lambda (path)
-     (message path)
-     (let ((pyenv-version-path (f-expand ".python-version" path)))
-       (if (f-exists? pyenv-version-path)
-          (progn
-            (setq pyenv-current-version (s-trim (f-read-text pyenv-version-path 'utf-8)))
-            (pyenv-mode-set pyenv-current-version)
-            (pyvenv-workon pyenv-current-version)
-            (message (concat "Setting virtualenv to " pyenv-current-version))))))))
+    (lambda (path)
+      (message path)
+      (let ((pyenv-version-path (f-expand ".python-version" path)))
+	(if (f-exists? pyenv-version-path)
+	    (progn
+      	      (setq pyenv-current-version (s-trim (f-read-text pyenv-version-path 'utf-8)))
+      	      (pyenv-mode-set pyenv-current-version)
+      	      (pyvenv-workon pyenv-current-version)
+      	      (message (concat "Setting virtualenv to " pyenv-current-version))))))))
 
-(add-hook 'after-init-hook 'pyenv-init)
-(add-hook 'projectile-after-switch-project-hook 'pyenv-activate-current-project)
 
 (provide 'lang-python)
 ;;; base-python.el ends here
