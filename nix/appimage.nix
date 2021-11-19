172218ebcbb8ac4a933eb50e305634c42a8b1cb5
Erik Oosting <erik99.oosting@gmail.com>
2021-11-19 20:39:50 +0100

    make emacs read my packages again

--- nix/appimage.nix
+++ nix/appimage.nix
@@ -1,8 +1,9 @@
 { pkgs ? import ./.,  sources ? import ./sources.nix }:
 let
-  emacs-ng = pkgs.emacsWithPackagesFromUsePackage {
+  emacsConfig = pkgs.runCommand "config.el" {} ''cat ${../elisp}/*.el > $out'';
+  emacs-ng = pkgs.emacsWithPackagesFromUsePackage { # Why won't emacs-overlay grab my packages?
     package = (import sources.emacs-ng).outputs.defaultPackage.${builtins.currentSystem};
-    config = "${../.}/init.el";
+    config = builtins.readFile emacsConfig.outPath;
     alwaysEnsure = true;
     extraEmacsPackages = e: [
       emacs-webkit
