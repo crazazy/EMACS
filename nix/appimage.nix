069ddc2735d64e88084f8e088d0503f10900dd8e
Erik Oosting <erik99.oosting@gmail.com>
2021-11-19 13:10:18 +0100

    add appimage creation file

---
+++ nix/appimage.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import ./.,  sources ? import ./sources.nix }:
+let
+  emacs-ng = pkgs.emacsWithPackagesFromUsePackage {
+    package = (import sources.emacs-ng).outputs.defaultPackage.${builtins.currentSystem};
+    config = "${../.}/init.el";
+    alwaysEnsure = true;
+    extraEmacsPackages = e: [
+      emacs-webkit
+    ];
+  };
+  emacs-webkit = import sources.emacs-webkit { inherit pkgs; };
+  # emacs = emacs-ng.epkgs.withPackages (e: [emacs-webkit e.evil]);
+  bundle = import "${sources.nix-bundle}/appimage-top.nix" { nixpkgs' = sources.nixpkgs; }; 
+in
+with bundle; appimage (appdir {name = "emacs"; target = emacs-ng;})
