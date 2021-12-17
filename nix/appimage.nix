e3f7b8b5e7398fa4c1221b16227120aa61517882
Erik Oosting <erik99.oosting@gmail.com>
2021-12-17 17:20:09 +0100

    turned emacs-webkit compilation into a function

--- nix/appimage.nix
+++ nix/appimage.nix
@@ -1,19 +1,20 @@
 { pkgs ? import ./.,  sources ? import ./sources.nix }:
 let
   emacsConfig = pkgs.runCommand "config.el" {} ''cat ${../elisp}/*.el > $out'';
-  emacs-ng = pkgs.emacsWithPackagesFromUsePackage { # Why won't emacs-overlay grab my packages?
-    package = (import sources.emacs-ng).outputs.defaultPackage.${builtins.currentSystem};
+  emacs-ng = (import sources.emacs-ng).outputs.defaultPackage.${builtins.currentSystem};
+  emacsDist = pkgs.emacsWithPackagesFromUsePackage { # Why won't emacs-overlay grab my packages?
+    package = emacs-ng;
     config = builtins.readFile emacsConfig.outPath;
     alwaysEnsure = true;
     extraEmacsPackages = e: [
-      emacs-webkit
+      (emacs-webkit-f e)
     ];
   };
-  emacs-webkit = import sources.emacs-webkit { inherit pkgs; };
-  # emacs = emacs-ng.epkgs.withPackages (e: [emacs-webkit e.evil]);
+  emacs-webkit-f = p: import sources.emacs-webkit { inherit pkgs; inherit (p) trivialBuild; };
+  emacs = emacs-ng.pkgs.withPackages (e: [(emacs-webkit-f e) e.evil]);
   bundle = import "${sources.nix-bundle}/appimage-top.nix" { nixpkgs' = sources.nixpkgs; }; 
 in
 with bundle; {
-  result = appimage (appdir {name = "emacs"; target = emacs-ng;});
-  inherit emacs-ng emacs-webkit;
+  result = appimage (appdir {name = "emacs"; target = emacsDist;});
+  inherit emacs emacsDist emacs-webkit;
 }
